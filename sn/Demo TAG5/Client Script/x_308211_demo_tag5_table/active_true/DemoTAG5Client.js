84040e34e49ea329420c46f29ea1563b0512c573
System Administrator <admin@example.com>
2019-12-11 09:06:41 -0500

    Changes by 'System Administrator' on 'Demo TAG5 - 1.0.2' -- Build #1

---
+++ sn/Demo TAG5/Client Script/x_308211_demo_tag5_table/active_true/DemoTAG5Client.js
@@ -0,0 +1,20 @@
+/* 
+ * Application : Demo TAG5
+ * ClassName   : sys_script_client
+ * Created On  : 2019-12-11 13:38:52
+ * Created By  : admin
+ * Updated On  : 2019-12-11 14:03:09
+ * Updated By  : admin
+ * URL         : /sys_script_client.do?sys_id=fc93de9bdb210010343f58b3ca96191e
+ */
+/**
+ * Description
+ * 
+ * @author System Administrator [admin]
+ * @memberof x_308211_demo_tag5.module:sys_script_client
+ * @returns {undefined}
+ */
+function onChange(){
+	var test3;
+	
+}
