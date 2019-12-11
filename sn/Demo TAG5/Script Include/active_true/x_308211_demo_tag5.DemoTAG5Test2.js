de5f8a538c63e5f5ecc72895d2febede921ec919
System Administrator <admin@example.com>
2019-12-11 09:15:32 -0500

    Changes by 'System Administrator' on 'Demo TAG5 - 1.0.5' -- Build #1

---
+++ sn/Demo TAG5/Script Include/active_true/x_308211_demo_tag5.DemoTAG5Test2.js
@@ -0,0 +1,28 @@
+/* 
+ * Application : Demo TAG5
+ * ClassName   : sys_script_include
+ * Created On  : 2019-12-11 14:15:01
+ * Created By  : admin
+ * Updated On  : 2019-12-11 14:15:07
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=e1eb5617db610010343f58b3ca9619eb
+ */
+/**
+ * Class Description
+ * 
+ * @class 
+ * @author System Administrator [admin]
+ * @memberof x_308211_demo_tag5.module:sys_script_include
+ */
+var DemoTAG5Test2 = Class.create();
+DemoTAG5Test2.prototype = /** @lends x_308211_demo_tag5.module:sys_script_include.DemoTAG5Test2.prototype */ {
+    /**
+     * Description
+     * 
+     * @returns {undefined}
+     */
+    initialize: function() {
+    },
+
+    type: 'DemoTAG5Test2'
+};
